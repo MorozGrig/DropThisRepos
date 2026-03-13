6cafd40ba47f9cb292fe14131c1452470135bd15
fredd <freed567890@mail.ru>
2026-03-13 11:22:50 +0300

    kik

--- obj/Release/net9.0/DropThisSite.AssemblyInfo.cs
+++ obj/Release/net9.0/DropThisSite.AssemblyInfo.cs
@@ -11,11 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DropThisSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2a23dba3653daeaec368e376deda2e2b7cbac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb76c606f2e1d302f4cf00589f172bf2899fd927")]
 [assembly: System.Reflection.AssemblyProductAttribute("DropThisSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DropThisSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
